86ae2546533853866ac8e812e03b608c617aa97c
reneeb <github@renee-baecker.de>
2014-12-17 09:02:19 +0100

    prepare release 1.28

--- lib/OTRS/OPM/Maker/Command/sopm.pm
+++ lib/OTRS/OPM/Maker/Command/sopm.pm
@@ -20,7 +20,7 @@ use OTRS::OPM::Maker -command;
 use OTRS::OPM::Maker::Utils::OTRS3;
 use OTRS::OPM::Maker::Utils::OTRS4;
 
-our $VERSION = 1.27;
+our $VERSION = 1.28;
 
 sub abstract {
     return "build sopm file based on metadata";
