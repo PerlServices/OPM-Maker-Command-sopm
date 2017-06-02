2b95301232c327cb17c21de42058a413bbc522d7
reneeb <info@perl-services.de>
2017-06-02 08:51:16 +0200

    prepare release

--- lib/OTRS/OPM/Maker/Command/sopm.pm
+++ lib/OTRS/OPM/Maker/Command/sopm.pm
@@ -22,7 +22,7 @@ use OTRS::OPM::Maker -command;
 use OTRS::OPM::Maker::Utils::OTRS3;
 use OTRS::OPM::Maker::Utils::OTRS4;
 
-our $VERSION = 1.38;
+our $VERSION = 1.39;
 
 sub abstract {
     return "build sopm file based on metadata";
